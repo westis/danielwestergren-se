794b30c508ee1f58293d23fed5284303588dec93
Daniel Westergren <westis@gmail.com>
2024-10-03 13:57:19 +0200

    Visual Editing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/sanity", "@nuxt/icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/sanity",
+    "@nuxt/icon",
+  ],
   sanity: {
     projectId: "p8xdhqgt",
     dataset: "production",
     useCdn: true,
+    visualEditing: {
+      token: process.env.NUXT_SANITY_VISUAL_EDITING_TOKEN,
+      studioUrl: process.env.NUXT_SANITY_VISUAL_EDITING_STUDIO_URL,
+    },
   },
   shadcn: {
     /**
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -4,6 +4,11 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemaTypes";
 import { iconify } from "sanity-plugin-iconify";
 import { structure } from "./deskStructure";
+import { presentationTool } from "sanity/presentation";
+
+const SANITY_STUDIO_PREVIEW_URL =
+  process.env.SANITY_STUDIO_PREVIEW_URL ||
+  "https://danielwestergrense.netlify.app";
 
 export default defineConfig({
   name: "default",
@@ -20,6 +25,13 @@ export default defineConfig({
     iconify({
       collections: ["fa", "mdi", "heroicons"],
       showName: false,
+    }),
+    presentationTool({
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
+      title: "Visual Preview",
+      name: "visual-preview",
+      // You can add a custom icon if desired
+      // icon: YourCustomIcon,
     }),
   ],
 
